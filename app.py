b378c919298c1d2f1ab31710030c0d3d4d5c3c14
soporteagroavicola <soportetecnico@agroavicoladelllano.com>
2026-08-10 16:34:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1217,3 +1217,4 @@ with app.app_context(): init_db()
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
